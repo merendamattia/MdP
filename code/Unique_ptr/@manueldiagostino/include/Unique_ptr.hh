20f1d8a0a92e63256407f1f1b11ebffc9ff75501
manueldiagostino <diagostinomanuel@gmail.com>
2023-04-22 17:43:39 +0200

    Modifiche Unique_ptr @manueldiagostino

--- code/Unique_ptr/@manueldiagostino/include/Unique_ptr.hh
+++ code/Unique_ptr/@manueldiagostino/include/Unique_ptr.hh
@@ -1,10 +1,10 @@
-/* 
-* Obiettivo: 
-*	- definire la classe Unique che implementa gli unique_ptr 
-* 	dello standard c++11.
-*	- seguire protocollo RAII
-* 
-*/
+/*
+ * Created on Sat Apr 22 2023
+ * by Di Agostino Manuel
+ * <manuel.diagostino@studenti.unipr.it>
+ * <https://github.com/manueldiagostino>
+ */
+
 
 #ifndef UNIQUE_HH
 #define UNIQUE_HH
@@ -23,17 +23,25 @@ private:
 
 public:
 	Unique_ptr() noexcept;
+	explicit Unique_ptr(const pointer&) = delete;
 	explicit Unique_ptr(pointer&) noexcept;
 	explicit Unique_ptr(pointer&&) noexcept; // rvalue a quel puntatore in quanto non possono esserci altri proprietari
 	Unique_ptr(const Unique_ptr&) = delete;
-	Unique_ptr(Unique_ptr&&) = delete;
+	Unique_ptr(Unique_ptr&&) noexcept;
 
 	Unique_ptr& operator=(const Unique_ptr&) = delete;
 	Unique_ptr& operator=(Unique_ptr&&) noexcept;
 	~Unique_ptr() noexcept;
 
 	// Functions
+	pointer	release() noexcept;
+	void reset(pointer&& = nullptr) noexcept;
+	void swap(Unique_ptr&) noexcept;
+
+	const value_type& operator *() const noexcept;
+	pointer operator ->() const noexcept;
 	operator bool() const noexcept;
+	pointer get() const noexcept;
 };
 
 
@@ -49,19 +57,24 @@ template<typename T>
 Unique_ptr<T>::Unique_ptr() noexcept :
 	data_{nullptr} {
 	
+	assert(data_ == nullptr);
+
 	#ifdef DEBUG
-	std::cout << "Unique_ptr(): Oggetto creato" << std::endl;
+	std::cerr << "Unique_ptr(): Oggetto creato" << std::endl;
 	#endif
 }
 
 template<typename T>
 Unique_ptr<T>::Unique_ptr(pointer& p) noexcept :
 	data_{p} {
-	
+
 	p = nullptr;
 
+	assert(data_ != nullptr);
+	assert(p == nullptr);
+
 	#ifdef DEBUG
-	std::cout << "Unique_ptr(pointer& p): Oggetto creato" << std::endl;
+	std::cerr << "Unique_ptr(pointer& p): Oggetto creato" << std::endl;
 	#endif
 }
 
@@ -71,8 +84,23 @@ Unique_ptr<T>::Unique_ptr(pointer&& p) noexcept :
 	
 	p = nullptr;
 
+	assert(data_ != nullptr);
+	assert(p == nullptr);
+
 	#ifdef DEBUG
-	std::cout << "Unique_ptr(pointer&& p): Oggetto creato" << std::endl;
+	std::cerr << "Unique_ptr(pointer&& p): Oggetto creato" << std::endl;
+	#endif
+}
+template<typename T>
+Unique_ptr<T>::Unique_ptr(Unique_ptr&& other) noexcept :
+	data_{std::move(other.data_)} {
+	
+	other.data_ = nullptr;
+	
+	assert(other.data_ == nullptr);
+
+	#ifdef DEBUG
+	std::cerr << "operator=(Unique_ptr&& other): Oggetto assegnato per spostamento" << std::endl;
 	#endif
 }
 
@@ -83,8 +111,10 @@ Unique_ptr<T>::operator=(Unique_ptr&& other) noexcept {
 	data_ = other.data_;
 	other.data_ = nullptr;
 
+	assert(other.data_ == nullptr);
+
 	#ifdef DEBUG
-	std::cout << "operator=(Unique_ptr&& other): Oggetto assegnato per spostamento" << std::endl;
+	std::cerr << "operator=(Unique_ptr&& other): Oggetto assegnato per spostamento" << std::endl;
 	#endif
 
 	return *this;
@@ -96,13 +126,78 @@ Unique_ptr<T>::~Unique_ptr() noexcept {
 	data_ = nullptr;
 
 	#ifdef DEBUG
-	std::cout << "Oggetto eliminato" << std::endl;
+	std::cerr << "Oggetto eliminato" << std::endl;
 	#endif // DEBUG
 }
 
 template<typename T>
-Unique_ptr<T>::operator bool() const noexcept {
-	return data_ != nullptr;
+T*
+Unique_ptr<T>::release() noexcept {
+	pointer temp = data_;
+	data_ = nullptr;
+	return temp;
+
+	#ifdef DEBUG
+	std::cerr << "release(): puntatore rilasciato" << std::endl;
+	#endif
 }
+
+template<typename T>
+void
+Unique_ptr<T>::reset(pointer&& other) noexcept {
+	delete data_;
+	data_ = other;
+	
+	#ifdef DEBUG
+	std::cerr << "reset(): reset del puntatore" << std::endl;
+	#endif
+}
+
+template<typename T>
+void
+Unique_ptr<T>::swap(Unique_ptr& other) noexcept {
+	Unique_ptr temp = std::move(*this);
+	*this = std::move(other);
+	other = std::move(temp);
+
+	#ifdef DEBUG
+	std::cerr << "swap(): swap tra *this e other" << std::endl;
+	#endif
+}
+
+/**
+ * @brief Provides access to the object owned by `*this`.
+ * 
+ * If `get() == nullptr` the behavior is undefined. 
+ * @return the content handle by the pointer.
+ */
+template<typename T>
+const T&
+Unique_ptr<T>::operator *() const noexcept {
+	return *data_; 
+}
+
+/**
+ * @brief Provides access to the object owned by `*this`.
+ * @return the pointer to the object handled by `*this`.
+ */
+template<typename T>
+T*
+Unique_ptr<T>::operator ->() const noexcept {
+	return data_; 
+}
+
+template<typename T>
+Unique_ptr<T>::operator bool() const noexcept {
+	return (data_ != nullptr) ? true : false;
+}
+
+template<typename T>
+T*
+Unique_ptr<T>::get() const noexcept {
+	return data_;
+}
+
+
 
 #endif //! UNIQUE_HH
